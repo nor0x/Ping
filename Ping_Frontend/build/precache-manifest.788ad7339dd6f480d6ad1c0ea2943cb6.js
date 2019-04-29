cd23a10263a2cfa6410e17fadadfee0fc23342a6
JaeYeopHan <ljyhanll@gmail.com>
2019-04-28 22:54:30 -0700

    [frontend] build

---
+++ Ping_Frontend/build/precache-manifest.788ad7339dd6f480d6ad1c0ea2943cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cd34cc94e7313d209541a0bd9aeef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a687a32b768b8f64abf0",
+    "url": "/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "d9bd7d6f1d0e62e37606",
+    "url": "/static/css/main.522e962d.chunk.css"
+  },
+  {
+    "revision": "a687a32b768b8f64abf0",
+    "url": "/static/js/2.35c76271.chunk.js"
+  },
+  {
+    "revision": "d9bd7d6f1d0e62e37606",
+    "url": "/static/js/main.cdd41046.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
