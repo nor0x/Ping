cb9d6c27e7641238a9c141441a9c9cebb0bfd27c
JaeYeopHan <ljyhanll@gmail.com>
2019-04-29 16:10:14 -0700

    [frontend] Fix subtitle

--- Ping_Frontend/src/components/header/SubTitle.jsx
+++ Ping_Frontend/src/components/header/SubTitle.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-import './SubTitle.css'
+import "./SubTitle.css";
 
-export const SubTitle = () => <h3 className="sub-title">Ping is blablabla</h3>;
+export const SubTitle = () => (
+  <h3 className="sub-title">
+    Global directory of areas where the demand of help of organizations is in real need
+  </h3>
+);
