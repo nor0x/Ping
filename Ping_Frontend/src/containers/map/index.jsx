a14efa87fe9fde4c3da3d330ed5c0e9e765ee88e
JaeYeopHan <ljyhanll@gmail.com>
2019-04-28 22:02:41 -0700

    [frontend] Add markers to map

--- Ping_Frontend/src/containers/map/index.jsx
+++ Ping_Frontend/src/containers/map/index.jsx
@@ -7,23 +7,31 @@ import {
   Marker
 } from "react-google-maps";
 
+import "./index.css";
+
 export const MapContainer = compose(
   withProps({
     googleMapURL:
       "https://maps.googleapis.com/maps/api/js?key=AIzaSyCJpMlKrjQ6G9Jf7I8jp1RObNp4C7LvRJc&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ margin: `32px 0px`,height: `100%` }} />
+    mapElement: <div style={{ margin: `32px 0px`, height: `100%` }} />
   }),
   withScriptjs,
   withGoogleMap
 )(props => (
   <GoogleMap defaultZoom={8} defaultCenter={{ lat: 37.33939, lng: -121.89496 }}>
-    {props.isMarkerShown && (
-      <Marker
-        position={{ lat: 37.33939, lng: -121.89496 }}
-        onClick={props.onMarkerClick}
-      />
-    )}
+    <Marker
+      position={{ lat: 37.33739, lng: -121.89496 }}
+      onClick={props.onMarkerClick}
+    />
+    <Marker
+      position={{ lat: 37.33939, lng: -121.89696 }}
+      onClick={props.onMarkerClick}
+    />
+    <Marker
+      position={{ lat: 37.33919, lng: -121.89196 }}
+      onClick={props.onMarkerClick}
+    />
   </GoogleMap>
 ));
