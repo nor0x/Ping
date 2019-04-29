b0025a4458819268f85b1740b11ab12a69c09783
Joachim Leonfellner <nor0x@users.noreply.github.com>
2019-04-29 07:44:28 -0700

    calling api via https

--- Ping_Frontend/src/api/index.js
+++ Ping_Frontend/src/api/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://pinghack.azurewebsites.net/api/Ping";
+export const API_BASE_URL = "https://pinghack.azurewebsites.net/api/Ping";
 
 // 1. typing address
 // 2. Generate lat, lng in front
 // 3. send data to backend (lat, lng)
 
 export function addPing(payload) {
-  return axios.post(API_BASE_URL, payload)
+  return axios.post(API_BASE_URL, payload);
 }
